0f5a4ed3f5741103a935e2d1787457a92dcbcb8a
agentN86 <agentn86@duck.com>
2024-12-19 18:50:39 -0600

    thing

--- scripts/tools.js
+++ scripts/tools.js
@@ -82,3 +82,26 @@ function cloakTemplate(name) {
             break;
     }
 };
+
+function submit() {
+    const input = document.getElementById('abtblankurl').value;
+    var win = window.open();
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
+    win.document.title = "New Tab";
+    var iframe = win.document.createElement('iframe');
+    iframe.style.border = 'none';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.margin = '0';
+    iframe.referrerpolicy = "no-referrer";
+    iframe.src = input;
+    win.document.body.appendChild(iframe);
+}
+
+const aboutbln = document.getElementById('abtblankurl');
+aboutbln.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        submit();
+    };
+});
